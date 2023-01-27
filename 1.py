442783d7fbfa8e45c37f6fc738c945dd35508bd6
ADITYACS121 <119475155+ADITYACS121@users.noreply.github.com>
2023-01-27 21:02:01 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+marks=int(input("ENTER MARKS "))
+if marks<25:
+    print("Grade:F")
+elif marks>=25 and marks<45:
+    print("Grade:E")
+elif marks>=45 and marks<50:
+    print("Grade:D")
+elif marks>=50 and marks<60:
+    print("Grade:C")
+elif marks>=60 and marks<80:
+    print("Grade:B")
+elif marks>80 and marks<=100:
+    print("Grade:A")
+else:
+    print("Invalid Marks entered")
